0789afa23a0b51a8ac9274e2277c11bc42e0fd80
jareste <jareste2000@gmail.com>
2024-09-30 22:31:05 +0200

    added makefile and main just with the initial concept

---
+++ Makefile
@@ -0,0 +1,66 @@
+NAME = ft_strace
+
+#########
+RM = rm -rf
+CC = cc
+CFLAGS = -Werror -Wextra -Wall -g -fsanitize=address
+LDFLAGS = -lm
+RELEASE_CFLAGS = $(CFLAGS) -DNDEBUG
+#########
+
+#########
+FILES = main 
+
+SRC = $(addsuffix .c, $(FILES))
+
+vpath %.c srcs
+#########
+
+#########
+OBJ_DIR = objs
+OBJ = $(addprefix $(OBJ_DIR)/, $(SRC:.c=.o))
+DEP = $(addsuffix .d, $(basename $(OBJ)))
+#########
+
+#########
+$(OBJ_DIR)/%.o: %.c
+	@mkdir -p $(@D)
+	${CC} -MMD $(CFLAGS) -c -Isrcs -Iinc $< -o $@
+
+all: .gitignore
+	$(MAKE) $(NAME)
+
+$(NAME): $(OBJ) Makefile
+	$(CC) $(CFLAGS) $(OBJ) -o $(NAME) $(LDFLAGS)
+	@echo "EVERYTHING DONE  "
+#	@./.add_path.sh
+
+release: CFLAGS = $(RELEASE_CFLAGS)
+release: re
+	@echo "RELEASE BUILD DONE  "
+
+clean:
+	$(RM) $(OBJ) $(DEP)
+	$(RM) -r $(OBJ_DIR)
+	@echo "OBJECTS REMOVED   "
+
+fclean: clean
+	$(RM) $(NAME)
+	@echo "EVERYTHING REMOVED   "
+
+re:	fclean all
+
+.gitignore:
+	@if [ ! -f .gitignore ]; then \
+		echo ".gitignore not found, creating it..."; \
+		echo ".gitignore" >> .gitignore; \
+		echo "$(NAME)" >> .gitignore; \
+		echo "$(OBJ_DIR)/" >> .gitignore; \
+		echo ".gitignore created and updated with entries."; \
+	else \
+		echo ".gitignore already exists."; \
+	fi
+
+.PHONY: all clean fclean re release .gitignore
+
+-include $(DEP)
---
+++ srcs/main.c
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_SYSCALL_NAME    32
+#define MAX_SYSCALL_NUMBER  512
+
+typedef struct {
+    int number;
+    char name[MAX_SYSCALL_NAME];
+} Syscall;
+
+typedef enum {
+    false = 0,
+    true = 1
+} bool;
+
+Syscall syscalls_64[MAX_SYSCALL_NUMBER];
+Syscall syscalls_32[MAX_SYSCALL_NUMBER];
+
+/*
+    Print the list of syscalls with their number and name.
+    Just for debug purposes.
+*/
+void print_syscall_list()
+{
+    int i;
+    bool first = true;
+
+    printf("Syscalls 64 bits\n");
+    for (i = 0; i < MAX_SYSCALL_NUMBER; i++)
+    {
+        if (syscalls_64[i].number == 0 && !first)
+            break;
+
+        first = false;
+
+        printf("%d: %s\n", syscalls_64[i].number, syscalls_64[i].name);
+    }
+
+    first = true;
+    printf("Syscalls 32 bits\n");
+    for (i = 0; i < MAX_SYSCALL_NUMBER; i++)
+    {
+        if (syscalls_32[i].number == 0 && !first)
+            break;
+
+        first = false;
+        printf("%d: %s\n", syscalls_32[i].number, syscalls_32[i].name);
+    }
+}
+
+/*
+    As ptrace just informs a number, we need the relation between the number and the name of the syscall.
+    This function reads the unistd_64.h and unistd_32.h files to get the syscall number and name.
+*/
+void init_syscall_list()
+{
+    FILE *fp = fopen("/usr/include/x86_64-linux-gnu/asm/unistd_64.h", "r");
+    char line[256];
+    char syscall_name[MAX_SYSCALL_NAME];
+    int syscall_number;
+    int syscall_count = 0;
+
+    if (fp)
+    {
+        while (fgets(line, sizeof(line), fp))
+        {
+            if (strncmp(line, "#define __NR_", 13) == 0)
+            {
+                sscanf(line, "#define __NR_%31s %d", syscall_name, &syscall_number);
+
+                if (syscall_number < MAX_SYSCALL_NUMBER)
+                {
+                    syscalls_64[syscall_count].number = syscall_number;
+                    strncpy(syscalls_64[syscall_count].name, syscall_name, MAX_SYSCALL_NAME);
+                    // printf("%d: %s\n", syscalls_64[syscall_count].number, syscalls_64[syscall_count].name);
+                    syscall_count++;
+                }
+            }
+        }
+
+        fclose(fp);
+    }
+
+    syscall_count = 0;
+    fp = fopen("/usr/include/x86_64-linux-gnu/asm/unistd_32.h", "r");
+    if (fp)
+    {
+        while (fgets(line, sizeof(line), fp))
+        {
+            if (strncmp(line, "#define __NR_", 13) == 0)
+            {
+                sscanf(line, "#define __NR_%31s %d", syscall_name, &syscall_number);
+
+                if (syscall_number < MAX_SYSCALL_NUMBER)
+                {
+                    syscalls_32[syscall_count].number = syscall_number;
+                    strncpy(syscalls_32[syscall_count].name, syscall_name, MAX_SYSCALL_NAME);
+                    syscall_count++;
+                }
+            }
+        }
+
+        fclose(fp);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    pid_t child;
+
+    /* No program to trace */
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <command> [args]\n", argv[0]);
+        return 1;
+    }
+
+    /*
+        Init both structures with list of syscalls to be used later.
+    */
+    init_syscall_list(&syscalls_64, &syscalls_32);
+
+    /* TODO: Remove */
+    print_syscall_list(&syscalls_64, &syscalls_32);
+    (void)syscalls_64;
+    (void)syscalls_32;
+    return 0;
+    /* TODO: Remove */
+
+    /*
+        Fork where we will run the program that will be traced.
+    */
+    child = fork();
+    if (child == 0)
+    {
+        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1)
+        {
+            perror("ptrace(PTRACE_TRACEME)");
+            return 1;
+        }
+
+        /* TODO: build path */
+        execvp(argv[1], &argv[1]);
+        perror("execvp");
+        exit(1);
+    }
+    else if (child > 0)
+    {
+        /* TODO: implement traces*/
+    }
+    else
+    {
+        fprintf(stderr, "Fatal error. fork failed: %s\n", strerror(errno));
+        return 1;
+    }
+
+    return 0;
+}
